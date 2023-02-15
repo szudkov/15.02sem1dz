8b61d137d46d1c2ff9d924a3c7cb83ddac31a704
Sergy <zudkov@ya.ru>
2023-02-15 21:08:29 +0300

    pervoe zadan

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите первое число");
+int pervoe = int.Parse(Console.ReadLine ());
+Console.WriteLine("Введите второе число");
+int vtoroe = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int tretie = int.Parse(Console.ReadLine ());
+int Max = pervoe;
+if (pervoe > vtoroe)
+ {
+  if (pervoe > tretie)
+  Max = pervoe;
+  else
+  Max = tretie;
+}
+if (pervoe < vtoroe)
+ {
+  if (vtoroe > tretie)
+   Max = vtoroe;
+   else 
+   Max = tretie;
+ }
+ Console.WriteLine("Максимальное число равно " + Max );
